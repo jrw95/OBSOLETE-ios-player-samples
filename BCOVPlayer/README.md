64e453a953b6321da149df45d27cb9e8183ae48e
BCJwhisenant <jwhisenant@brightcove.com>
2014-09-08 16:12:32 -0400

    Move SampleBCOVPlayer into a new directory (BCOVPlayer).

---
+++ BCOVPlayer/README.md
@@ -0,0 +1,1 @@
+A set of sample applications that use the Brightcove Player SDK in conjunction with the Brightcove IMA plugin.
---
+++ BCOVPlayer/objc/BCOVPlayer/BCOVPlayer/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  BCOVPlayer
+//
+// Copyright (c) 2014 Brightcove, Inc. All rights reserved.
+// License: https://accounts.brightcove.com/en/terms-and-conditions
+//
+
+@import UIKit;
+
+#import "BCOVPlayerSDK.h"
+
+#import "VideoStillView.h"
+
+
+@interface ViewController : UIViewController <BCOVPlaybackControllerDelegate>
+
+@end
